3d2ee9217fcb40ea1797abd6ef6a9df43af4bd35
Cari Palazzolo <cari.pizza@gmail.com>
2018-10-29 15:31:06 -0700

    updated todo-list to include onRemove and build list functionality

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 import addTodo from './add-todo';
-// import todoList from './todo-list.js';
+import todoList from './todo-list.js';
 import todoApi from './todo-api.js';
 
 const todos = todoApi.getAll();
 // console.log(todos);
 
 todoList.init(todos, function(todo) {
-    //  onRemove listener
     
     todoApi.remove(todo);
 });
---
+++ js/todo-list.js
@@ -0,0 +1,48 @@
+
+
+function makeTodo(todo) {
+    const html = /*html*/`
+    <li class="todo">
+        <h3 class="name" style="background: ${todo.color};">
+            ${todo.name}
+        </h3>
+        <img src="assets/${todo.image ? todo.image : 'assets/caripizza.jpeg'}">
+        <button class="danger">X</button>
+    </li>
+    `;
+
+    // template element to convert string to DOM
+    const template = document.createElement('template');
+
+    // B. Assign innerHTML property
+    template.innerHTML = html;
+    return template.content;
+}
+
+const list = document.getElementById('todos');
+
+const todoList = {
+
+    init(todos, onRemove) {
+        for(let i = 0; i < todos.length; i++) {
+            todoList.add(todos[i]);
+        }
+        todoList.onRemove = onRemove;
+    },
+    add(todo) {
+        const dom = makeTodo(todo);
+
+        const removeButton = dom.querySelector('button');
+        const listItem = dom.querySelector('li');
+
+        removeButton.addEventListener('click', function() {
+            todoList.onRemove(todo);
+            listItem.remove();
+        });
+
+        // append to <ul>, this will empty the fragment
+        list.appendChild(dom);
+    }
+};
+
+export default todoList;
