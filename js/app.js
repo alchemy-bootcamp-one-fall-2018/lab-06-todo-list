d07caa7dd998afb48fdb407041185ee76c7285c4
Abel Quintero <abel.j.quintero@gmail.com>
2018-10-29 16:43:14 -0700

    app adds onRemove callback function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 import addTodo from './add-todo.js';
 import todoList from './todo-list.js';
+import todoApi from './todo-api.js';
+
+const todoItems = todoApi.getAll();
+
+todoList.init(todoItems, function(task) {
+    //the onRemove listener
+
+    todoApi.remove(task);
+});
 
 addTodo.init(function(task) {
     
+    // tell the api service
+    todoApi.add(task);
+
+    //then update components
     todoList.add(task);
 });
