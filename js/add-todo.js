355654a3c6460e5bb8566f037cc0c57ef48d3b30
Carmen V Ramos <carmenvramos@yahoo.com>
2018-10-29 15:47:20 -0700

    Added addTodo Component

---
+++ js/add-todo.js
@@ -0,0 +1,27 @@
+const addTodo = {
+  // "init" method for initializing the component
+  // 1. passed data or callback functions
+  // 2. setup some event event listeners
+    init(onAdd) {
+        const form = document.getElementById('add-form');
+
+  // add event listeners
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+      
+      // form elements
+            const elements = form.elements;
+
+      // get the values from the form controls
+            const todo = {
+                task: elements.task.value,
+                dueDate: elements.dueDate.value
+            };
+
+            onAdd(todo);
+
+        });
+    }
+};
+
+export default addTodo;
