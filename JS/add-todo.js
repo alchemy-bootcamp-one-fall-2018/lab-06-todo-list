f1077879d8476e8c78d172da4242e8c24c3dc487
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-10-29 14:29:40 -0700

    wrote the add-task in add-todo.js

---
+++ JS/add-todo.js
@@ -0,0 +1,22 @@
+const addTask = {
+    init(onAdd) {
+        const form = document.getElementById('add-task');
+
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+
+            const elements = form.elements;
+
+            const task = {
+                task: 'Finish lab',
+                due: new Date('10/30/2018')
+            };
+            onAdd(task);
+
+            form.reset();
+            document.activeElement.blur();
+        });
+    }
+};
+
+export default addTask;
