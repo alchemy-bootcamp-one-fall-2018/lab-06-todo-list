e5b415a689947ffae6f88ff50ba6cd9238394d30
Abel Quintero <abel.j.quintero@gmail.com>
2018-10-29 16:09:45 -0700

    todoList adds DOM for new todo to <ul> parent

--- js/todo-list.js
+++ js/todo-list.js
@@ -10,8 +10,43 @@ function makeTodoList(todo){
     </li>
 `;
 
+//uses template to create DOM for new todo
 
+    const template = document.createElement('template');
+    
+    template.innerHTML = html;
 
+    return template.content;
 }
-//eslint-disable-next-line
+
+//creates new list variable 
+const list = document.getElementById('tasks');
+
+const todoList = {
+    //initialize tasks array and calls back onRemove
+    init(tasks, onRemove){
+        for(let i = 0; i < tasks.length; i++) {
+            todoList.add(tasks[i]);
+        }
+        todoList.onRemove = onRemove;
+    },
+    add(task){
+        const dom = makeTodoList(task);
+
+        //find the elements before appending
+        const removeButton = dom.querySelector('button');
+        const listItem = dom.querySelector('li');
+
+        removeButton.addEventListener('click', function() {
+            todoList.onRemove(task);
+            listItem.remove();
+        });
+
+        //append to <ul> to empty the fragment
+        list.appendChild(dom);
+    }
+};
+
+
+
 export default todoList;
