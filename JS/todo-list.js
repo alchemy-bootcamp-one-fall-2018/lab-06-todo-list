eec312afe8035dd321c6f2bf315e0e7060de1d83
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-10-29 16:48:00 -0700

    wprled oun fnc makeTask in todo-list.js

---
+++ JS/todo-list.js
@@ -0,0 +1,10 @@
+function makeTask(task) {
+    const html = /*html*/`
+        <h3 class="task" >
+        </h3>
+        <h3 class="${task.due ? Date.now : task.due}"> 
+        </h3>
+        
+    `;    
+const template = document
+}
