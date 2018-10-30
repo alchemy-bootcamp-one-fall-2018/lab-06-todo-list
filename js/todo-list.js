af8d00fddc168023bce39d3c849a498a9e022d15
Connor Frendt <cjacksonfx@yahoo.com>
2018-10-29 17:21:12 -0700

    got add button working

---
+++ js/todo-list.js
@@ -0,0 +1,40 @@
+function makeToDo(toDo) {
+    const html = /*html*/`
+        <li class="toDo">
+            <h3 class="name">
+                ${toDo.task}
+                ${toDo.due}
+            </h3>
+            <button class="danger">X</button>
+        </li>
+    `;
+
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template.content;
+}
+
+const list = document.getElementById('toDos');
+
+const toDoList = {
+    init(toDos, onRemove) {
+        for(let i = 0; i < toDos.length; i++) {
+            toDoList.add(toDos[i]);
+        }
+        toDoList.onRemove = onRemove;
+    },
+    add(toDo) {
+        const dom = makeToDo(toDo);
+        const removeButton = dom.querySelector('button');
+        const listItem = dom.querySelector('li');
+
+        removeButton.addEventListener('click', function() {
+            toDoList.onRemove(toDo);
+            listItem.remove();
+        });
+
+        list.appendChild(dom);
+    }
+};
+
+export default toDoList;
