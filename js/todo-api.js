96de717f023818e6126aaddb57f2c616cdcb80b5
Marcy Silverman <marcysilve225@gmail.com>
2018-10-29 17:11:36 -0700

    date is fixed and everything is running

--- js/todo-api.js
+++ js/todo-api.js
@@ -1,11 +1,11 @@
 let items = [
     {
         name: 'Go Grocery Shopping', 
-        date: new Date('10/30/2018')
+        date: new Date(Date.UTC(10, 18, 19)).toLocaleDateString()
     },
     {
         name: 'Take the Dog for a Walk',
-        date: new Date('10/30/2018')
+        date: new Date(Date.UTC(10, 18, 8)).toLocaleDateString()
     }
 ]; 
 
