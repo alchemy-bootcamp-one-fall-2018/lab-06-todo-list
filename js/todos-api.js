438c70d86b0cdb0c10efd8fc6fe44a1bc975fb9d
Kristin Hortsch <kristinhortsch@gmail.com>
2018-10-29 17:43:03 -0700

    mobile styling

--- js/todos-api.js
+++ js/todos-api.js
@@ -17,7 +17,7 @@ function saveTodos() {
 
 function customParser(key, value) {
     if(key !== 'date') return value;
-    return new Date(value);
+    return new Date(value).toLocaleDateString();
 }
 
 const todosApi = {
