a69130b7a64814217a013e9f10b60d32d47e0ed4
Shaba9 <shaba92@gmail.com>
2018-10-30 00:02:36 -0700

    attempt dates

--- js/app.js
+++ js/app.js
@@ -14,22 +14,23 @@ const el = form.elements;
 const taskList = document.getElementById('task-list');
 
 var data = JSON.parse(localStorage.getItem('todo'));
+var item = {};
 
 function display(){ 
     let li = '';
     
     for(let i = 0; i < data.length; i++){
-        li += '<li>' + data[i].task + '</li><button name="' + i + '">Remove</button>';
+        li += '<li class="' + item.classColor + '">' + data[i].task + '</li><button name="' + i + '">Remove</button>';
     }
     
     taskList.innerHTML = li;
 }
 
-taskList.addEventListener('click', function(event) {
-    let ind = event.target.name;
-    data.splice(ind);
-    display();
-});
+// taskList.addEventListener('click', function(event) {
+    //     let ind = event.target;
+    //     console.log(data.splice());
+//     display();
+// });
 
 if(localStorage.getItem('todo')){
 
@@ -38,15 +39,30 @@ if(localStorage.getItem('todo')){
     form.addEventListener('submit', function(event){
         event.preventDefault();
         
-        var item = {
+        item = {
             task: el.task.value,
             date: new Date(el.dueDate.value),
             complete: false
         };
+
+        let month = item.date.getMonth() + 1;
+        let day = item.date.getDate();
+        let year = item.date.getFullYear();
+        let today = new Date();
+        let tMonth = today.getMonth() + 1;
+        let tDay = today.getDate();
+        let tYear = today.getFullYear();
+
+        if(item.date >= today) {
+            item['classColor'] = 'green';
+        } else {
+            item['classColor'] = 'red';
+        }
+
+        console.log(item.classColor);
     
         data.push(item);
         localStorage.setItem('todo', JSON.stringify(data));
-        console.log(localStorage.getItem('todo'));
 
         display();
 
