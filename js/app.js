6639a8fa59a8e9e2e37f3da23cb3b400c56eb1c2
Shaba9 <shaba92@gmail.com>
2018-10-29 22:14:24 -0700

    json parses correctly

--- js/app.js
+++ js/app.js
@@ -12,24 +12,38 @@
 const form = document.getElementById('create-task');
 const el = form.elements;
 
+console.log('data initial', localStorage.getItem('todo'));
 form.addEventListener('submit', function(event){
     event.preventDefault();
     
-    var data = [localStorage.getItem('todo')];
-    console.log('data:', data);
-    
-    const item = {
+    var data = JSON.parse(localStorage.getItem('todo'));
+    console.log('data initial:', data);
+
+    var item = {
         task: el.task.value,
         date: new Date(el.dueDate.value),
         complete: false
     };
 
-    if(data[0]) {
-        data.push(item);
-    } else {
-        data[0] = item;
-    }
-    console.log('data: ', data);
+    item = JSON.stringify(item);
 
+    if(data) {
+        data += ',' + item;
+        
+    } else {
+        data = item;
+    }
+
+    data = JSON.stringify(data);
+    
+    // for(let i = 0; i < data.length; i++) {
+        //     console.log('loop:', data[i]);
+        // }
+        
     localStorage.setItem('todo', data);
+    console.log('data final', data);
 });
+
+var a = '{ "name":"John", "age":30, "city":"New York"}';
+var b = JSON.parse(a);
+console.log(b);
