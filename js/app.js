05a5f2f059d2319b33cb3243af63cefc4cf77680
Connor Frendt <cjacksonfx@yahoo.com>
2018-11-05 17:31:26 -0800

    Made to do lists render to page as well as the form

--- js/app.js
+++ js/app.js
@@ -1,14 +1,47 @@
-import addToDo from './add-todo.js';
+import AddToDo from './add-todo.js';
 import toDoApi from './todo-api.js';
-import toDoList from './todo-list.js';
+import ToDoList from './todo-list.js';
+import html from './html.js';
 
 const toDos = toDoApi.getAll();
 
-toDoList.init(toDos, function(toDo) {
-    toDoApi.remove(toDo);
-});
+function makeTemplate() {
+    return html`
+    <main>
+        <section id="add-form">
+            <h2><u>Add a Task:</u></h2>
+        </section>
+            
+        <section id="toDos">
+            <h2><u>List of Tasks:</u></h2>
+        </section>
 
-addToDo.init(function(toDo) {
-    toDoApi.add(toDo);
-    toDoList.add(toDo);
-});
+    </main>
+    `;
+}
+
+class App {
+    render() {
+        const dom = makeTemplate();
+        const addToDoSection = dom.getElementById('add-form');
+        const toDoListSection = dom.getElementById('toDos');
+
+        const addToDo = new AddToDo(toDo => {
+            toDoApi.add(toDo);
+            toDoList.add(toDo);
+        });
+
+        const toDoList = new ToDoList(toDos, toDo => {
+            toDoApi.remove(toDo);
+        });
+
+        addToDoSection.appendChild(addToDo.render());
+        toDoListSection.appendChild(toDoList.render());
+
+        return dom;
+    }
+}
+
+const app = new App();
+
+document.getElementById('root').appendChild(app.render());
